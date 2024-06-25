93987b831b8569a4c4a51c61db8c020f6f1c79bb
Ricardo López Cruz <230110088@itsoeh.edu.mx>
2024-06-24 23:58:26 -0600

    Correccion en histograma de limites

--- Graphics_Lims/plotHistograma.py
+++ Graphics_Lims/plotHistograma.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_grafHist(frecuencias_relativas, marcas_clase, marcas_texto):
+def plot_grafHist(frecuencias_relativas, marcas_clase, marcas_texto): 
     plt.figure(figsize=(12, 6))
-    plt.bar(marcas_clase, frecuencias_relativas, width=1, edgecolor="k", color=["#37CA64", "#FF99F3", "#37C5CA", "#E74E50", "#FC8C29"])
+    x = np.arange(len(marcas_clase))  # Creamos un arreglo de indices para las columnas
+    width = 0.5  # Ancho de las columnas
     
-    plt.xticks(marcas_clase, marcas_texto, fontsize=15, rotation=45)
+    plt.bar(x, frecuencias_relativas, width, edgecolor="k", color=["#37CA64", "#FF99F3", "#37C5CA", "#E74E50", "#FC8C29"])
+    
+    plt.xticks(x, marcas_texto, fontsize=15, rotation=45)
     plt.xlabel("Marcas de clase", fontsize=20)
     plt.ylabel("Frecuencia relativa", fontsize=20)
     plt.title("Histograma", fontsize=25)
-    plt.ylim(0, 6) 
+    plt.ylim(0, max(frecuencias_relativas) * 1.1) 
 
     plt.grid()
     plt.show()
